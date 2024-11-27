63795e34f063066dd2c3467e3f6f67ed1c97a067
69205208+NomarGomez@users.noreply.github.com <69205208+NomarGomez@users.noreply.github.com>
2024-11-27 16:45:58 -0400

    Modal basic done

---
+++ src/components/Modals/Modal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useRef } from "react";
+import { FaXmark } from "react-icons/fa6";
+
+export const Modal = ({ isOpen, onClose, children }) => {
+  const modalRef = useRef(null);
+
+  const closeModal = () => {
+    onClose();
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div
+      ref={modalRef}
+      className=" fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      onClick={(e) => {
+        if (modalRef.current === e.target) {
+          closeModal();
+        }
+      }}
+    >
+      <div className="bg-white p-8 rounded shadow-lg flex flex-col ">
+        {children}
+      </div>
+
+    </div>
+  );
+};
